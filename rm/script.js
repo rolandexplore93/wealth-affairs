229b3640d5f48405b6718b6e0b3a3aa9f3de8a3f
rolandexplore93 <roland2rule@gmail.com>
2023-02-10 23:30:40 +0000

    Comment out close modal onwindow click functionality

--- rm/script.js
+++ rm/script.js
@@ -34,8 +34,8 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
