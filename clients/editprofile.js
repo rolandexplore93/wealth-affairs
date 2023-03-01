39407b222f0435dc4a10c6da147be49d5d3440db
ifecoy <61954384+ifecoy@users.noreply.github.com>
2023-03-01 17:39:32 +0000

    Edit profile

---
+++ clients/editprofile.js
@@ -0,0 +1,80 @@
+
+// function basic insturments checkbox
+var expanded = false;
+
+function BshowCheckboxes() {
+  var checkboxes = document.getElementById("bInstrument");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+
+// function deriavatives insturments checkbox
+var expanded = false;
+
+function DshowCheckboxes() {
+  var checkboxes = document.getElementById("derivative");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+// function risk level checkbox
+var expanded = false;
+
+function RshowCheckboxes() {
+  var checkboxes = document.getElementById("riskLevel");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+// function deriavatives insturments
+var expanded = false;
+
+function DshowCheckboxes() {
+  var checkboxes = document.getElementById("derivative");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+// function deriavatives insturments
+var expanded = false;
+
+function DshowCheckboxes() {
+  var checkboxes = document.getElementById("derivative");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+// function deriavatives insturments
+var expanded = false;
+
+function DshowCheckboxes() {
+  var checkboxes = document.getElementById("derivative");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
