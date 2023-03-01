1ac068a41c04bb1a488406c485958e6a4469b39c
rolandexplore93 <roland2rule@gmail.com>
2023-03-01 17:53:01 +0000

    Creating industry feature

--- rm/script.js
+++ rm/script.js
@@ -29,6 +29,10 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
+// document.getElementById('product-view').addEventListener("click", () => {
+//   document.getElementById("myModal").style.display = "block";
+// })
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
