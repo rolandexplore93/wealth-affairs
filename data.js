329ee9d4b56f6feb100ac56907ae952ac9ee98f3
rolandexplore93 <roland2rule@gmail.com>
2023-03-14 22:02:57 +0000

    Fetch list of countries

---
+++ data.js
@@ -0,0 +1,18 @@
+fetch('/Supporting-data-for-investment-idea-setup.csv')
+  .then(response => response.text())
+  .then(data => {
+    console.log(data);
+    const countries = data.split('\n');
+    // Remove the last empty row if necessary
+    if (countries[countries.length - 1] === '') {
+      countries.pop();
+    }
+    // Add the country names to the <select> element
+    const select = document.getElementById('country');
+    for (const country of countries) {
+      const option = document.createElement('option');
+      option.value = country;
+      option.textContent = country;
+      // select.appendChild(option);/
+    }
+});
