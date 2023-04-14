5e373a1c03871d5bea499ebf30526d7264cdaa34
rolandexplore93 <roland2rule@gmail.com>
2023-04-14 18:48:56 +0000

    Code cleansing

--- auth/validateLogin.js
+++ auth/validateLogin.js
@@ -2,7 +2,6 @@ fetch('http://localhost/wealth_affairs/auth/isLoggedIn.php')
 .then(response => response.json())
 .then(data => {
     console.log(data)
-    // let userRole = document.getElementById('title-role').innerHTML = (data.Role == 'RM' ? 'RMM00' : 'RMMMMMM');
     let userRole = document.getElementById('title-role').innerHTML = (data.Role == 'RM' && `Relationship Manager` || data.Role == 'FA' && 'Funds Administrator');
     let name = document.getElementById('name').innerHTML = (data.Role == 'RM' && `${data.Firstname} ${data.Lastname}` || data.Role == 'FA' && `${data.Firstname} ${data.Lastname}`);
     let email = document.getElementById('email').innerHTML = (data.Role == 'RM' && `${data.Email}` || data.Role == 'FA' && `${data.Email}`);
